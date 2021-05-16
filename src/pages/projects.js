c18df499b2ab3e1fded1208498bf234bccc9d015
George Cope <G.Cope13@gmail.com>
2021-05-16 17:20:23 +0100

    Created basic component, mapped ProjectData to ProjectCard & passed in props

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,27 @@ import ProjectCard from '../components/ProjectCard/index';
 
 const Projects = () => {
     return (
-
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 mx-auto center'>
+                    <h2>Portfolio Projects</h2>
+                </div>
+            </div>
+            <div className='row'>
+                {ProjectData.map((data) => {
+                    return (
+                        <ProjectCard 
+                            key={data.id}
+                            title={data.title}
+                            description={data.description}
+                            image={data.image}
+                            repository={data.repository}
+                            URL={data.URL}
+                        />
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
