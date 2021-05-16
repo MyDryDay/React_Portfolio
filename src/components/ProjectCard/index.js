6e42cb0e0183dbcb0045305ed2bfc0eb25ffc573
George Cope <G.Cope13@gmail.com>
2021-05-16 17:52:37 +0100

    Replacing plain URLs with icons

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { CgWebsite } from 'react-icons/cg';
 
 const ProjectCard = (props) => {
     return (
-        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-6 mx-auto'>
+        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-6 projectCard mx-auto'>
             <div class='mx-auto projectContainer'>
                 <img src={props.image} alt={props.title} className='img-fluid rounded projectImg'></img>
                 <div className='overlay'>
                     <div className='headerText'>{props.title}</div>
-                    <p>{props.description}</p>
-                    <a href={props.repository} className='linkRepo' target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
-                    <a href={props.URL} className='linkApp' target='_blank' rel='noopener noreferrer'>Live URL</a>
+                    <a href={props.repository} className='linkRepo' target='_blank' rel='noopener noreferrer'> <AiOutlineGithub /> </a>
+                    <a href={props.URL} className='linkApp' target='_blank' rel='noopener noreferrer'> <CgWebsite /> </a>
                 </div>
             </div>
+            <p className='description'>{props.description}</p>
         </div>
     )
 }
