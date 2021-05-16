e95ae01a4899acbcc45c6ab1ed49b06ce2e074ed
George Cope <G.Cope13@gmail.com>
2021-05-16 17:52:15 +0100

    Adding classes to elements

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,12 +4,14 @@ import ProjectCard from '../components/ProjectCard/index';
 
 const Projects = () => {
     return (
-        <div className='container-fluid'>
+        <div className='container-fluid projectSection'>
+        <br></br>
             <div className='row'>
                 <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12 mx-auto center'>
-                    <h2>Portfolio Projects</h2>
+                    <h2>Portfolio Projects <span className='blink'>_</span> </h2>
                 </div>
             </div>
+            <br></br>
             <div className='row'>
                 {ProjectData.map((data) => {
                     return (
