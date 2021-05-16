2893a6eb445e0c5e619beb63ac23d6598b04534e
George Cope <G.Cope13@gmail.com>
2021-05-16 17:19:08 +0100

    Added basic structure to ProjectCard index file, just so something can be rendered

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
     return (
-
+        <div className='col-xs-8 col-sm-8 col-md-8 col-lg-6 mx-auto'>
+            <div class='mx-auto'>
+                <img src={props.image} alt={props.title} className='img-fluid rounded'></img>
+                <div className='overlay'>
+                    <div className='headerText'>{props.title}</div>
+                    <p>{props.description}</p>
+                    <a href={props.repository} target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
+                    <a href={props.URL} target='_blank' rel='noopener noreferrer'>Live URL</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
