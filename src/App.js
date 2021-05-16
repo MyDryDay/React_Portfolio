14bd8278721bfdeeb74bc79c0a7b08cf57f8a548
George Cope <G.Cope13@gmail.com>
2021-05-16 17:20:57 +0100

    Added in router path for 'projects section'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar/index';
 
 import About from './pages/about';
+import Projects from './pages/projects';
 
 import './App.css';
 
@@ -17,6 +18,9 @@ function App() {
         </Route>
         <Route exact path='/about'>
           <About />
+        </Route>
+        <Route exact path='/projects'>
+          <Projects />
         </Route>
       </Switch>
       </div>
