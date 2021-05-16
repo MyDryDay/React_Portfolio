3aafb68e3bc44374d80bc835929545fccc3c8298
George Cope <G.Cope13@gmail.com>
2021-05-16 13:16:31 +0100

    Fixed spelling error & import issues with icons, some reformatting & added links to contact me

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {
     AiOutlineMail,
     AiOutlineGithub,
-    AiOutlineLinkedIn
+    AiOutlineLinkedin
 } from 'react-icons/ai';
-import FaTelegramPlane from 'react-icons/fa';
+import { FaTelegramPlane } from 'react-icons/fa';
 import displayImage from '../images/JapanTrip1.jpg';
 
 const About = () => {
@@ -12,10 +12,12 @@ const About = () => {
         <div>
             <div className='container-fluid'>
                 <div className='row'>
-                    <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                        <div class="h-100 p-5 text-white bg-dark">
-                            <h2 className='center'>George Cope, Web-Developer <span className='blink'>_</span> </h2>
-                            <img src={displayImage} className='mx-auto d-block img-fluid rounded-circle img' alt='Osaka. Japan'></img>
+                    <div className='aboutSection'>
+                        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
+                            <div className="h-100 p-5 m-0 text-white bg-dark">
+                                <h2 className='center'>George Cope, Web-Developer <span className='blink'>_</span> </h2>
+                                <img src={displayImage} className='mx-auto d-block img-fluid rounded-circle img' alt='Osaka, Japan'></img>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -27,7 +29,7 @@ const About = () => {
                     <br></br>
                         <h3 className='center'>About Me</h3>
                         <br></br>
-                        <div className='objectDiv col-xs-12 col-sm-6 col-md-6 col-lg-6'>
+                        <div className='objectDiv col-xs-8 col-sm-8 col-md-8 col-lg-6 mx-auto'>
                             &#123; 
                                 <p className='tab'>
                                     <span className='objKey'>name</span>: <span className='objString'>"George Cope"</span>, <br></br>
@@ -36,11 +38,16 @@ const About = () => {
                                     <span className='objKey'>age</span>: <span className='objInt'>23</span>, <br></br>
                                     <span className='objKey'>occupation</span>: <span className='objString'>"Student"</span>, <br></br>
                                     <span className='objKey'>interests</span>: [<span className='objString'>"reading"</span>, <span className='objString'>"walking"</span>, <span className='objString'>"gaming"</span>, <span className='objString'>"puzzles"</span>], <br></br>
+                                    <span className='objKey'>contact</span>: &#123;
+                                        <p className='tab'>
+                                            <span className='objKey'>#1</span>: <a href='https://www.linkedin.com/in/george-cope-633b761bb/' className='objLink' target='_blank' rel='noopener noreferrer'>LinkedIn <AiOutlineLinkedin /></a> <br></br>
+                                            <span className='objKey'>#2</span>: <a href='https://github.com/MyDryDay' className='objLink' target='_blank' rel='noopener noreferrer'>GitHub <AiOutlineGithub /></a> <br></br>
+                                            <span className='objKey'>#3</span>: <a href='https://t.me/G_Cope97' className='objLink' target='_blank' rel='noopener noreferrer'>Telegram <FaTelegramPlane /></a> <br></br>
+                                            <span className='objKey'>#4</span>: <a href='mailto: g.cope13@gmail.com' className='objLink' target='_blank' rel='noopener noreferrer'>Email <AiOutlineMail /></a>
+                                        </p>
+                                    &#125;
                                 </p>
                             &#125; <span className='blink'>_</span>
-                        </div>
-                        <div className='statementDiv col-xs-12 col-sm-6 col-md-6 col-lg-6'>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptatum ipsum excepturi enim nemo tempore pariatur corporis ipsam necessitatibus harum. Facere minus, consequuntur itaque provident eius laboriosam ipsa libero beatae.</p>
                         </div>
                     </div>
                 </div>
