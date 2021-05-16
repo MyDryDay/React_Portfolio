bfdbcee74b5ae3095151c535ea53f0f60d1db0fb
George Cope <G.Cope13@gmail.com>
2021-05-16 13:57:48 +0100

    Created a component to hold portfolio data, imported screenshots & created template objects to be filled in

---
+++ src/components/ProjectData/index.js
@@ -0,0 +1,50 @@
+import BudgetTracker from '../../images/projects/BudgetTracker.png';
+import CodeQuiz from '../../images/projects/CodeQuiz.png';
+import EmployeeDirectory from '../../images/projects/EmployeeDirectory.png';
+import FFS from '../../images/projects/FFS.png';
+import PasswordGen from '../../images/projects/PasswordGen.png';
+
+const portfolio = [
+    {
+        id: ,
+        title: ,
+        description: ,
+        image: ,
+        repository: ,
+        URL: 
+    },
+    {
+        id: ,
+        title: ,
+        description: ,
+        image: ,
+        repository: ,
+        URL: 
+    },
+    {
+        id: ,
+        title: ,
+        description: ,
+        image: ,
+        repository: ,
+        URL: 
+    },
+    {
+        id: ,
+        title: ,
+        description: ,
+        image: ,
+        repository: ,
+        URL: 
+    },
+    {
+        id: ,
+        title: ,
+        description: ,
+        image: ,
+        repository: ,
+        URL: 
+    }
+]
+
+export default portfolio;
