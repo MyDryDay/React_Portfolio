b3a34c5fd0e4e090d973b961368096283e889798
George Cope <G.Cope13@gmail.com>
2021-05-16 14:13:14 +0100

    Created a template for the file with the anticipated imports

---
+++ src/pages/projects.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ProjectData from '../components/ProjectData/index';
+import ProjectCard from '../components/ProjectCard/index';
+
+const Projects = () => {
+    return (
+
+    )
+}
+
+export default Projects;
