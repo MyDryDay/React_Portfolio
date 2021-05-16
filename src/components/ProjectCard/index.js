d554ab0c2d701d9567564e9fdc9c825c036a7d50
George Cope <G.Cope13@gmail.com>
2021-05-16 17:26:48 +0100

    Added styling throughout projects page

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -3,13 +3,13 @@ import React from 'react';
 const ProjectCard = (props) => {
     return (
         <div className='col-xs-8 col-sm-8 col-md-8 col-lg-6 mx-auto'>
-            <div class='mx-auto'>
-                <img src={props.image} alt={props.title} className='img-fluid rounded'></img>
+            <div class='mx-auto projectContainer'>
+                <img src={props.image} alt={props.title} className='img-fluid rounded projectImg'></img>
                 <div className='overlay'>
                     <div className='headerText'>{props.title}</div>
                     <p>{props.description}</p>
-                    <a href={props.repository} target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
-                    <a href={props.URL} target='_blank' rel='noopener noreferrer'>Live URL</a>
+                    <a href={props.repository} className='linkRepo' target='_blank' rel='noopener noreferrer'>GitHub Repo</a>
+                    <a href={props.URL} className='linkApp' target='_blank' rel='noopener noreferrer'>Live URL</a>
                 </div>
             </div>
         </div>
